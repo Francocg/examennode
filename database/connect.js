712f73aa08249b9d940a2159caed4bc31b21a388
francocg <francocabanillas@upeu.edu.pe>
2021-06-08 18:38:18 -0500

    examen

---
+++ database/connect.js
@@ -0,0 +1,11 @@
+const database = {
+    user: "uhwmnuzuuetht3nk74fi",
+    host: "b1r9xompscr7euc55awx-postgresql.services.clever-cloud.com",
+    password: "VIY2TKmDdmPe4TU9kcYE",
+    database: "b1r9xompscr7euc55awx",
+    port: 5432
+  }
+  
+  module.exports = {
+      database
+  }
